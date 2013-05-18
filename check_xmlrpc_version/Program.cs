3c143178edd8f4eb729adad7f5356fdeac9ae231
DIENST-JE <jens.dienst@wacker.com>
2013-05-18 10:45:50 +0200

    add check_xmlrpc_version subproject

---
+++ check_xmlrpc_version/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace check_xmlrpc_version
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
